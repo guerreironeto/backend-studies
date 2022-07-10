08ef0bdc468b72098aaad8bc612790dbd1d705ed
Francisco Lourenço Guerreiro Neto <guerreironeto13@gmail.com>
2022-07-10 05:12:57 -0300

    exercícios e funções

---
+++ template-strings.js
@@ -0,0 +1,9 @@
+const nome = "Neto";
+const idade = 2022-1989;
+const cidadeDeNascimento = "FOR";
+
+//const apresentacao = "meu nome é " + nome + ", minha idade é " + idade + ", nasci na cidade de " + cidadeDeNascimento;
+
+const apresentacao = `meu nome é ${nome}, minha idade é ${idade} e nasci na cidade de ${cidadeDeNascimento}`;
+
+console.log(apresentacao)
